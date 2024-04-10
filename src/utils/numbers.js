6944bb0aa7f92632edb957a2dbe42aa62efdc3a0
Erfan Afzali <developer.invoker@gmail.com>
2024-04-10 05:53:46 +0330

    write 3 comment

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -1,8 +1,11 @@
+// english to persion convert
 const e2p = (s) => s.toString().replace(/\d/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d]);
 
+// persion to english convert
 const p2e = (s) =>
   s.toString().replace(/[۰-۹]/g, (d) => "۰۱۲۳۴۵۶۷۸۹".indexOf(d));
 
+// text like 10000000 => 100,000,000
 const sp = (number) => {
   const seperatedNumber = number
     .toString()
